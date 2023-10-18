2ffe21b207eeee4b4ee051249bcb4b0c04c0aaa9
Alhaanahmed-duet <Alhaanahmed123@gmail.com>
2023-10-18 14:01:51 +0500

    Files are updated

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
+    var preLoaderShow = () => {
+        preloader.style.display = "block";
+    }
+    var preloaderHide = () => {
+        preloader.style.display = "none";
+    }
     var preloader = document.getElementById("showloading");
     window.addEventListener("load", function() {
-        preloader.style.display = "none";
+        preloaderHide(); // After loading loading screen will not display
     })
     document.querySelector(".linkfunc").addEventListener("click", function () {
-        preloader.style.display = "block";
+        preLoaderShow(); // This is meant to show Loading Screen
         var xhttp = new XMLHttpRequest();
         
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState === 4 && xhttp.status === 200) {
 
-                preloader.style.display = "none";                
+                preloaderHide();   // This is meant to hide the preloader after http request reponse i.e AJAX Response               
                 document.getElementById("main").innerHTML = xhttp.responseText;
             }
         };
         xhttp.open("GET", "snippets/snippets.html", true);
         xhttp.send();
 
-}) })
+})
+        // Rest of the Code Goes Here
+
+
+
+
+
+
+})
